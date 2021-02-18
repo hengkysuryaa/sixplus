b7b1b5cad2f726387ebeb7cfb98cf2a436aaf5a9
hengkysuryaa <hengkysurya88@gmail.com>
2021-02-18 15:28:02 +0700

    implementasi model LO

--- LO/models.py
+++ LO/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class LO(models.Model):
+    lo_a = models.CharField(max_length=1)
+    lo_b = models.CharField(max_length=1)
+    lo_c = models.CharField(max_length=1)
+    lo_d = models.CharField(max_length=1)
+    lo_e = models.CharField(max_length=1)
+    lo_f = models.CharField(max_length=1)
