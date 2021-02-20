c817e4121166720e9f73c4381fb7bb7dece7a101
hengkysuryaa <hengkysurya88@gmail.com>
2021-02-20 20:33:40 +0700

    #25 Implementasi Model Mata Kuliah dan Kuliah

--- LO/models.py
+++ LO/models.py
@@ -1,10 +1,33 @@
 from django.db import models
 
 # Create your models here.
+class Course(models.Model):
+    course_id = models.CharField(max_length=6, unique=True)
+    title = models.CharField(max_length=100)
+    dept_name = models.CharField(max_length=10)
+    credits = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.course_id}"
+
 class LO(models.Model):
+    course_id = models.OneToOneField("Course", on_delete=models.CASCADE)
     lo_a = models.CharField(max_length=1)
     lo_b = models.CharField(max_length=1)
     lo_c = models.CharField(max_length=1)
     lo_d = models.CharField(max_length=1)
     lo_e = models.CharField(max_length=1)
     lo_f = models.CharField(max_length=1)
+    lo_g = models.CharField(max_length=1)
+
+    def __str__(self):
+        return f"LO {self.course_id}"
+
+class Section(models.Model):
+    course_id = models.ForeignKey(Course, on_delete=models.CASCADE)
+    sec_id = models.IntegerField()
+    semester = models.IntegerField()
+    year = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.course_id}, K{self.sec_id}, {self.semester}-{self.year}"
