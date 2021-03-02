92b836bc095335f1f9929d6814ab04170386ddf4
unknown <gunawan48160@gmail.com>
2021-03-02 14:13:37 +0700

    #44 Implementasi XLSX Utility + Add Converter

--- Utils/xlsxutil.py
+++ Utils/xlsxutil.py
@@ -1,17 +1,35 @@
 import pandas as pd
+import numpy as np 
+
+# Usage : convert_normal_array_to_pandas({'columnName' : array})
+def convert_normal_array_to_pandas(arrDict):
+	df = pd.DataFrame(columns = [*arrDict])
+	for key in arrDict:
+		df[key] = arrDict[key]
+	return df
+
+def convert_pandas_to_normal_array(df):	
+	columns = list(df) 
+
+	arrDict = {}
+	  
+	for column in columns: 
+	    arrDict[column] = df[column].to_list()
+
+	return arrDict
 
 def export_pandas_to_sheet(dataframe, file_name = "New Excel.xlsx", sheet_name = "New Sheet"):
-	dataframe.to_excel(file_name, sheet_name = sheet_name, index = false)
+	dataframe.to_excel(file_name, sheet_name = sheet_name, index = False)
 
 def export_pandas_to_workbook(sheets_reference, file_name = "New Excel.xlsx"):
 	xlsxWriter = pd.ExcelWriter(file_name, engine='xlsxwriter')
 	
 	for sheet_name in sheets_reference.keys():
-		sheets_reference[sheet_name].to_excel(writer, sheet_name = sheet_name, index = False)
+		sheets_reference[sheet_name].to_excel(xlsxWriter, sheet_name = sheet_name, index = False)
 
-	writer.save()
+	xlsxWriter.save()
 
-def import_workbook_as_pandas(file_name):
+def import_workbook_as_pandasDict(file_name):
 	# sheet_name = None means to read every sheet
 	return pd.read_excel(file_name, sheet_name=None)
 
