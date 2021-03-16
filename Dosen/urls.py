e895d52f43bbe102c85e839da2c52c196d213a1c
unknown <gunawan48160@gmail.com>
2021-03-16 07:25:59 +0700

    Download Upload XLSX

--- Dosen/urls.py
+++ Dosen/urls.py
@@ -5,5 +5,9 @@ from . import views
 
 urlpatterns = [url(r'^KomponenNilai/$', views.DistribusiKomponenNilaiView.as_view(), name='KomponenNilai'),
             url(r'^KomponenNilai/(?P<course_id>[A-Z0-9]+)/$', views.FormsDistribusiNilai, name='FormKomponen'),
-            url(r'^KomponenNilai/(?P<course_id>[A-Z0-9]+)/Result/$', views.SubmitView, name='Submit')
+            url(r'^KomponenNilai/(?P<course_id>[A-Z0-9]+)/Result/$', views.SubmitView, name='Submit'),
+            url(r'^KomponenNilai/(?P<course_id>[A-Z0-9]+)/Export/$', views.exportListMhs, name='Export'),
+            url(r'^KomponenNilai/(?P<course_id>[A-Z0-9]+)/Import/$', views.FormsImportNilai, name='FormImport'),
+            url(r'^KomponenNilai/(?P<course_id>[A-Z0-9]+)/Import/Berhasil/$', views.importListMhs, name='Import')
+
 ]
--- Dosen/views.py
+++ Dosen/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.views import generic
 from LO.models import LO, Course, BobotKomponenScore, Takes, Section
-from Utils.xlsxutil import export_pandas_to_sheet, convert_normal_array_to_pandas
+from Utils.xlsxutil import export_pandas_to_sheet, convert_normal_array_to_pandas, import_workbook_as_pandasDict
 from openpyxl import *
+from django.http import HttpResponse
+from django.utils.datastructures import MultiValueDictKeyError
 
 # Create your views here.
 class DistribusiKomponenNilaiView(generic.ListView):
@@ -38,4 +40,42 @@ def downloadListMhs(section):
     data = {'NIM':list_nim, 'Nama':list_nama, 'UTS1':[], 'UTS2':[], 'UAS':[], 'Kuis':[], 'Tutorial':[]}
     df = convert_normal_array_to_pandas(data)
     name = section.course_id.course_id + " K" + str(section.sec_id)
+
     export_pandas_to_sheet(df, "Lembar Penilaian " + name + ".xlsx", name)
+
+def exportListMhs(request, course_id):
+    #list_nim, list_nama = Takes.get_student_takes(Takes, section)
+    list_nim = ['13518103', '13518114']
+    list_nama = ['Gunawan', 'Kamaruddin']
+    data = {'NIM':list_nim, 'Nama':list_nama, 'UTS1':[], 'UTS2':[], 'UAS':[], 'Kuis':[], 'Tutorial':[]}
+    df = convert_normal_array_to_pandas(data)
+    #name = section.course_id.course_id + " K" + str(section.sec_id)
+    name = "MS1210" + "K5"
+
+    response = HttpResponse(
+        content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+    )
+    response['Content-Disposition'] = 'attachment; filename=Lembar Penilaian {name}.xlsx'.format(
+        name = name,
+    )
+
+    export_pandas_to_sheet(df, response, name)
+
+    return response
+
+def FormsImportNilai(request, course_id):
+    lo_list, course = LO.getCourseLO(LO, course_id)
+    return render(request, 'Dosen/import.html', {'course' : course})
+
+def importListMhs(request, course_id):
+    lo_list, course = LO.getCourseLO(LO, course_id)
+    try:
+        excel_file = request.FILES['fileToUpload']
+    except MultiValueDictKeyError:
+        return render(request, 'Dosen/import.html', {'course' : course})
+
+    if(str(excel_file).split('.')[-1] == "xlsx"):
+        dc = import_workbook_as_pandasDict(excel_file)
+        print(dc)
+
+    return render(request, 'Dosen/berhasil.html')
