2923f54b5668d386139035101787b88a84b53442
Gunawan Kamaruddin <13518103@std.stei.itb.ac.id>
2021-03-20 15:44:51 +0700

    Add Testcase

--- SIXPlus/settings.py
+++ SIXPlus/settings.py
@@ -102,6 +102,13 @@ DATABASES = {
     }
 }
 
+import sys
+if 'test' in sys.argv:
+    DATABASES['default'] = {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'mydatabase'
+    }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
