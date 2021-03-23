35040e085f1e971254467fa8cc7bb32657b43589
gaswicak <13518042@std.stei.itb.ac.id>
2021-03-23 09:37:34 +0700

    URL & View adjustment

--- Dosen/urls.py
+++ Dosen/urls.py
@@ -9,5 +9,8 @@ urlpatterns = [url(r'^OMM/$', LOView.as_view(), name='OMM'),
             url(r'^KomponenNilai/$', views.DistribusiKomponenNilaiView.as_view(), name='KomponenNilai'),
             url(r'^KomponenNilai/(?P<course_id>[A-Z0-9]+)/$', views.FormsDistribusiNilai, name='FormKomponen'),
             url(r'^KomponenNilai/(?P<course_id>[A-Z0-9]+)/Result/$', views.SubmitView, name='Submit'),
+            url(r'^KomponenNilai/(?P<course_id>[A-Z0-9]+)/Export/$', views.exportListMhs, name='Export'),
+            url(r'^KomponenNilai/(?P<course_id>[A-Z0-9]+)/Import/$', views.FormsImportNilai, name='FormImport'),
+            url(r'^KomponenNilai/(?P<course_id>[A-Z0-9]+)/Import/Berhasil/$', views.importListMhs, name='Import'),
             path('penilaian', penilaianPage, name = 'penilaian'),
 ]
