4471dfda72f5399944ecb962c4ca2ca9aaace960
hengkysuryaa <hengkysurya88@gmail.com>
2021-04-01 14:48:27 +0700

    #80 Mengambil identitas mahasiswa untuk form

--- LO/views.py
+++ LO/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import get_object_or_404
 
 from django.views import generic
@@ -25,25 +25,30 @@ class LOView(generic.ListView):
         return context
 
 def NextKerjasamaView(request, course_id):
-    namaPengisi = request.POST['name']
-    NIMPengisi = request.POST['NIM']
+    # namaPengisi = request.POST['name']
+    # NIMPengisi = request.POST['NIM']
+    identitas = request.user
     kel = request.POST['kelompok']
     penilaian = PenilaianKerjasamaForm()
-    return render(request, 'LO/next_form_kerjasama.html', {'nama':namaPengisi, 'nim':NIMPengisi, 'kel':kel, 'penilaian':penilaian, 'course_id':course_id})
+    return render(request, 'LO/next_form_kerjasama.html', {'identitas':identitas, 'kel':kel, 'penilaian':penilaian, 'course_id':course_id})
 
 def KerjasamaView(request, course_id):
     #Checking course_id nya valid gak
     course = get_object_or_404(Course, course_id=course_id)
     #TODO: Checking apakah dia takes matkul itu pada semester dan tahun itu(?)
 
-    identitas = IdentitasForm()
+    #identitas = IdentitasForm()
+    identitas = request.user
     penilaian = PenilaianKerjasamaForm()
 
     return render(request, 'LO/form_kerjasama.html', {'penilaian':penilaian, 'course_id': course_id, 'identitas': identitas})
 
 def SubmitKerjasamaView(request, course_id):
-    namaPengisi = request.POST['name']
-    NIMPengisi = request.POST['NIM']
+    # namaPengisi = request.POST['name']
+    # NIMPengisi = request.POST['NIM']
+    if (request.POST['NIMPeer'] == request.user.first_name):
+        return HttpResponseNotFound("<h2> Tidak bisa menilai diri anda sendiri!</h2> Silakan back ke laman sebelumnya")
+    identitas = request.user
     kel = request.POST['kelompok']
     
     student = get_object_or_404(Student, nim=request.POST['NIMPeer'])
@@ -51,7 +56,7 @@ def SubmitKerjasamaView(request, course_id):
     res = ResponseKerjasama(student=student, course=course, Kontribusi=int(request.POST['Kontribusi']), PemecahanMasalah=int(request.POST['PemecahanMasalah']), Sikap=int(request.POST['Sikap']), FokusTerhadapTugas=int(request.POST['FokusTerhadapTugas']), BekerjaDenganOrangLain=int(request.POST['BekerjaDenganOrangLain']))
     res.save()
 
-    return render(request, 'LO/form_kerjasama_submit.html', {'nama':namaPengisi, 'nim':NIMPengisi, 'kel':kel, 'course_id': course_id})    
+    return render(request, 'LO/form_kerjasama_submit.html', {'identitas':identitas, 'kel':kel, 'course_id': course_id})    
 
 def KomunikasiView(request, course_id):
     #Checking course_id nya valid gak
